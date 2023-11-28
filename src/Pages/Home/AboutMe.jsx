bb251f6e55cf2e461f3b7558d344db22a21ace99
Renato <renato92.hora@gmail.com>
2023-11-28 20:22:28 -0300

    Myportfolio e aboutme

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About me" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    {/* <p className="section--title">About</p> */}
+                    <h1 className="skills--section--heading">Sobre Mim</h1>
+                    <p className="hero--section-description">
+                        dddddddddddddddddddddddddddddd
+                    </p>
+                    <p className="hero--section-description">
+                        dddddddddddddddddddddddddddddd
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,15 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
 
 export default function Home () {
     return (
         <>
             <HeroSection />
-            <MySkills />
+            {/* <MySkills /> */}
+            <AboutMe />
+            <MyPortfolio />
         </>
     )
 }
